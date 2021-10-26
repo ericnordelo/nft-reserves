a2eea5e953d332a72cf7868f8a7909ce482925a6
cool-develope <cool199966@outlook.com>
2021-10-26 15:28:28 +0100

    remove Migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const PriviNFTBuying = artifacts.require("PriviNFTBuying");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(PriviNFTBuying);
 };
