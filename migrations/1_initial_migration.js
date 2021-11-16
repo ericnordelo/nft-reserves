2cfbd2c0b37f15258406026c7cb4849c11cd60bc
Eric Nordelo <eric.nordelo39@gmail.com>
2021-11-16 12:09:30 -0500

    feat: configuring hardhat

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const NFTReservalManager = artifacts.require("NFTReservalManager");
-
-module.exports = function (deployer) {
-  deployer.deploy(NFTReservalManager,
-    "0xad6d458402f60fd3b111111111131acdce07538d"
-  );
-};
