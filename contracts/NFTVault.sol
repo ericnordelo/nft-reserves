1bf2f1a7c5239fbf5b60e6f44c871cc608ff9676
Eric Nordelo <eric.nordelo39@gmail.com>
2021-11-16 12:41:49 -0500

    feat: get compiling code

--- contracts/NFTVault.sol
+++ contracts/NFTVault.sol
@@ -7,23 +7,29 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTVault is INFTVault {
     address public admin;
+
+    uint128 public immutable a = 1;
+    uint128 public immutable b = 1;
+    uint256 public immutable c = 1;
+    uint256 public constant k = 1;
+
     mapping(address => mapping(uint256 => address)) private _lockedNFTOwner;
 
     constructor(address _admin) {
         admin = _admin;
     }
 
-    function owner(address nft, uint256 nftID) external view returns (address) {
+    function owner(address nft, uint256 nftID) external view override returns (address) {
         return IERC721(nft).ownerOf(nftID);
     }
 
-    function lockNFT(address nft, uint256 nftID) external {
+    function lockNFT(address nft, uint256 nftID) external override {
         address owner_ = IERC721(nft).ownerOf(nftID);
         IERC721(nft).transferFrom(owner_, admin, nftID);
         _lockedNFTOwner[nft][nftID] = owner_;
     }
 
-    function unlockNFT(address nft, uint256 nftID) external {
+    function unlockNFT(address nft, uint256 nftID) external override {
         address owner_ = _lockedNFTOwner[nft][nftID];
         require(owner_ != address(0), "not locked NFT");
         IERC721(nft).transferFrom(admin, owner_, nftID);
@@ -34,7 +40,7 @@ contract NFTVault is INFTVault {
         address nft,
         uint256 nftID,
         address to
-    ) external {
+    ) external override {
         address owner_ = _lockedNFTOwner[nft][nftID];
         require(owner_ != address(0), "not locked NFT");
         IERC721(nft).transferFrom(admin, to, nftID);
--- contracts/mocks/PriceOracleMock.sol
+++ contracts/mocks/PriceOracleMock.sol
@@ -4,13 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import {IPriceOracle} from "../interface/IPriceOracle.sol";
 
 contract PriceOracleMock is IPriceOracle {
-  constructor() public {
-  }
-  function decimals(address) external view returns (uint256 _decimals) {
-      _decimals = 0;
-  }
+    function decimals(address) external pure override returns (uint256 _decimals) {
+        _decimals = 0;
+    }
 
-  function price(address) external view returns (uint256 price) {
-      price = 0;
-  }
+    function price(address) external pure override returns (uint256 _price) {
+        _price = 0;
+    }
 }
