b0e0f9e78e5e64ec291d9421b2134d7fe9037cdd
Eric Nordelo <eric.nordelo39@gmail.com>
2021-11-16 15:30:47 -0500

    feat: update upgrade task for new contracts

--- tasks/upgrade.js
+++ tasks/upgrade.js
@@ -5,7 +5,7 @@ task('upgrade', 'Upgrades an upgradeable contract')
     const { deployer } = await getNamedAccounts();
 
     const beacons = [];
-    const uups = ['OpenSalesManager'];
+    const uups = ['NFTVault'];
 
     const isBeacon = (cont) => {
       return beacons.includes(cont);
