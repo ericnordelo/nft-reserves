7a32129ed6b471fd6cc767a517bb377ba25d13f8
Eric Nordelo <eric.nordelo39@gmail.com>
2021-11-16 13:17:38 -0500

    feat: add full coverage to upgrades

--- contracts/NFTVault.sol
+++ contracts/NFTVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -10,8 +10,6 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
  * @dev the owner should be a governance contract because manage upgrades
  */
 contract NFTVault is UUPSUpgradeable, OwnableUpgradeable {
-    mapping(address => mapping(uint256 => address)) private _lockedNFTOwner;
-
     /**
      * @dev the initializer modifier is to avoid someone initializing
      *      the implementation contract after deployment
@@ -25,30 +23,6 @@ contract NFTVault is UUPSUpgradeable, OwnableUpgradeable {
         __Ownable_init();
         __UUPSUpgradeable_init();
     }
-
-    // function lockNFT(address nft, uint256 nftID) external {
-    //     address owner_ = IERC721(nft).ownerOf(nftID);
-    //     IERC721(nft).transferFrom(owner_, admin, nftID);
-    //     _lockedNFTOwner[nft][nftID] = owner_;
-    // }
-
-    // function unlockNFT(address nft, uint256 nftID) external {
-    //     address owner_ = _lockedNFTOwner[nft][nftID];
-    //     require(owner_ != address(0), "not locked NFT");
-    //     IERC721(nft).transferFrom(admin, owner_, nftID);
-    //     delete _lockedNFTOwner[nft][nftID];
-    // }
-
-    // function transferNFT(
-    //     address nft,
-    //     uint256 nftID,
-    //     address to
-    // ) external {
-    //     address owner_ = _lockedNFTOwner[nft][nftID];
-    //     require(owner_ != address(0), "not locked NFT");
-    //     IERC721(nft).transferFrom(admin, to, nftID);
-    //     delete _lockedNFTOwner[nft][nftID];
-    // }
 
     // solhint-disable-next-line no-empty-blocks
     function _authorizeUpgrade(address) internal view override onlyOwner {}
--- test/NFTVault_test.js
+++ test/NFTVault_test.js
@@ -1,5 +1,7 @@
 const NFTVault = artifacts.require('NFTVault');
 
+const { constants, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
+
 describe('NFTVault', function () {
   beforeEach(async () => {
     await deployments.fixture(['nft_vault']);
@@ -10,5 +12,24 @@ describe('NFTVault', function () {
 
   it('should be deployed', async () => {
     assert.isOk(this.vault.address);
+  });
+
+  describe('upgrade', () => {
+    it("can't upgrade with wrong accounts", async () => {
+      const { user } = await getNamedAccounts();
+
+      let revertMessage = 'Ownable: caller is not the owner';
+      await expectRevert(this.vault.upgradeTo(constants.ZERO_ADDRESS, { from: user }), revertMessage);
+    });
+
+    it('can upgrade with right account', async () => {
+      const { deployer } = await getNamedAccounts();
+
+      let newImplementation = await NFTVault.new();
+
+      let tx = await this.vault.upgradeTo(newImplementation.address, { from: deployer });
+
+      expectEvent(tx, 'Upgraded', { implementation: newImplementation.address });
+    });
   });
 });
